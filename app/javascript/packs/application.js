4725a5c99c1761483116b60419b3beacbfa4b179
Efe Agare <knowledgeagare157@gmail.com>
2023-06-19 19:46:02 +0100

    bundle webpack to pass test

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import 'stylesheets/application'
+import 'assets/stylesheets/application'
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
